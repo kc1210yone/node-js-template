671dce39e48310473e34923220adfb1bc254eb7d
Satoshi Yoneda <17329135+kc1210yone@users.noreply.github.com>
2026-04-26 15:55:03 +0900

    feat: a

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,5 +1,25 @@
 import type { Configuration } from 'lint-staged';
 
-export default {
-  '*': ['eslint --fix --no-warn-ignored', 'prettier --write --ignore-unknown'],
+const a = {
+  '!{apps,packages}/**': [
+    'eslint --fix --no-warn-ignored',
+    'prettier --write --ignore-unknown',
+  ],
+  ...Object.fromEntries(
+    [
+      'apps/next-js-example',
+      'apps/node-example',
+      'apps/vite-example',
+      'packages/shared',
+    ].map((workspace) => [
+      `${workspace}/**/*`,
+      [
+        `npx -w ${workspace} eslint --fix --no-warn-ignored`,
+        `npx -w ${workspace} prettier --write --ignore-unknown`,
+      ],
+    ]),
+  ),
 } satisfies Configuration;
+console.log(a);
+
+export default a;
